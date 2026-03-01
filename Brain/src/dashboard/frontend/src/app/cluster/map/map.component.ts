5c3eaef969de8223e15327a376ccd4e3fc906d03
cmh0728 <gambaro344@gmail.com>
2026-03-01 15:17:46 +0100

    edit map img size

--- Brain/src/dashboard/frontend/src/app/cluster/map/map.component.ts
+++ Brain/src/dashboard/frontend/src/app/cluster/map/map.component.ts
@@ -66,7 +66,7 @@ export class MapComponent {
   private mapY: number = 0;
 
   private screenSize = {"width": 100, "height": 100}; // screen size in %
-  private mapSize: number = 80; // map size in % for width
+  private mapSize: number = 50; // map size in % for width
   private mapWidth: number = 0;
   private mapHeight: number = 0;
 
