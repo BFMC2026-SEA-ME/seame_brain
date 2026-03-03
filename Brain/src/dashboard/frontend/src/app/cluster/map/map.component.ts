6a0ba25d4ece89b2a4c59d20a176acae6d8f10ab
cmh0728 <gambaro344@gmail.com>
2026-03-03 19:53:19 +0100

    edit cluster

--- Brain/src/dashboard/frontend/src/app/cluster/map/map.component.ts
+++ Brain/src/dashboard/frontend/src/app/cluster/map/map.component.ts
@@ -72,7 +72,7 @@ export class MapComponent {
     maxX: 732,
     maxY: 564
   };
-  private readonly mapFitPaddingRatio = 0.05;
+  private readonly mapFitPaddingRatio = 0.06;
 
   private screenSize = {"width": 100, "height": 100}; // screen size in %
   private mapSize: number = 50; // map size in % for width
