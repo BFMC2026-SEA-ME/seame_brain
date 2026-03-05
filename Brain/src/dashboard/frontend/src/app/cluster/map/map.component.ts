c1062e3d9bd6c6978f1ec025f68201a454db5695
cmh0728 <gambaro344@gmail.com>
2026-03-05 21:26:33 +0100

    edit node scale

--- Brain/src/dashboard/frontend/src/app/cluster/map/map.component.ts
+++ Brain/src/dashboard/frontend/src/app/cluster/map/map.component.ts
@@ -74,8 +74,8 @@ export class MapComponent {
   };
   private readonly mapFitPaddingRatio = 0.06;
   // Expand node spacing around map center to better match track geometry.
-  private readonly nodeSpreadScaleX = 1.06;
-  private readonly nodeSpreadScaleY = 1.06;
+  private readonly nodeSpreadScaleX = 1.07;
+  private readonly nodeSpreadScaleY = 1.03;
 
   private screenSize = {"width": 100, "height": 100}; // screen size in %
   private mapSize: number = 50; // map size in % for width
