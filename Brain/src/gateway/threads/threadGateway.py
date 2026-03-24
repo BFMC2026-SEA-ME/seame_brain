a1e6038482dbe9f48b0d34eeaf37e250f09a38c9
cmh0728 <gambaro344@gmail.com>
2026-03-24 15:19:44 +0100

    edit gateway

--- Brain/src/gateway/threads/threadGateway.py
+++ Brain/src/gateway/threads/threadGateway.py
@@ -48,6 +48,10 @@ class threadGateway(ThreadWithStop):
         self.sendingList = {}
         self.queuesList = queueList
         self.messageApproved = []
+        self._critical_batch_limit = 32
+        self._warning_batch_limit = 32
+        self._general_batch_limit = 128
+        self._config_batch_limit = 32
 
     # =================================== SUBSCRIBE ======================================
 
@@ -142,6 +146,48 @@ class threadGateway(ThreadWithStop):
     def _has_subscribers(self, owner, msg_id):
         return (owner, msg_id) in self.messageApproved
 
+    def _drain_queue(self, queue_name, limit):
+        processed = 0
+        queue_ref = self.queuesList.get(queue_name)
+        if queue_ref is None:
+            return 0
+
+        while processed < limit:
+            try:
+                message = queue_ref.get_nowait()
+            except Empty:
+                break
+
+            self.send(message)
+            processed += 1
+
+        return processed
+
+    def _process_config_messages(self, limit):
+        processed = 0
+        queue_ref = self.queuesList.get("Config")
+        if queue_ref is None:
+            return 0
+
+        while processed < limit:
+            try:
+                message = queue_ref.get_nowait()
+            except Empty:
+                break
+
+            try:
+                if str.lower(message["Subscribe/Unsubscribe"]) == "subscribe":
+                    self.subscribe(message)
+                else:
+                    self.unsubscribe(message)
+            except Exception as exc:
+                if self.debugging:
+                    self.logger.warning("Config routing failed: %r", exc)
+
+            processed += 1
+
+        return processed
+
     # ====================================================================================
 
     # Function for debugging:
@@ -156,27 +202,9 @@ class threadGateway(ThreadWithStop):
         """This function will take the messages in priority order form the queues.\n
         the prioirty is: Critical > Warning > General
         """
-        
-        # while self._running:
-        message = None
-        # We are using "elif" because we are processing one message at a time.
-        # We work with the queues in the priority order( We start from the high priority to low priority)
-        try:
-            message = self.queuesList["Critical"].get_nowait()
-        except Empty:
-            message = None
-        if message is None:
-            try:
-                message = self.queuesList["Warning"].get_nowait()
-            except Empty:
-                message = None
-        if message is None:
-            try:
-                message = self.queuesList["General"].get_nowait()
-            except Empty:
-                message = None
-        if message is not None:
-            self.send(message)
+        self._drain_queue("Critical", self._critical_batch_limit)
+        self._drain_queue("Warning", self._warning_batch_limit)
+        self._drain_queue("General", self._general_batch_limit)
 
         # Process latest image independently so camera frames are not starved
         # by a constantly non-empty General queue.
@@ -190,20 +218,7 @@ class threadGateway(ThreadWithStop):
                         break
                 if latest_image is not None:
                     self.send(latest_image)
-        if not self.queuesList["Config"].empty():
-            try:
-                message2 = self.queuesList["Config"].get_nowait()
-            except Empty:
-                message2 = None
-            if message2 is not None:
-                try:
-                    if str.lower(message2["Subscribe/Unsubscribe"]) == "subscribe":
-                        self.subscribe(message2)
-                    else:
-                        self.unsubscribe(message2)
-                except Exception as exc:
-                    if self.debugging:
-                        self.logger.warning("Config routing failed: %r", exc)
+        self._process_config_messages(self._config_batch_limit)
 
         # print(time.perf_counter_ns())
 
