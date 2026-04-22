762adf9d335590fab5480c2b487344bfefcd2496
cmh0728 <gambaro344@gmail.com>
2026-04-22 16:21:16 +0200

    edit speed error

--- Brain/src/hardware/serialhandler/threads/threadRead.py
+++ Brain/src/hardware/serialhandler/threads/threadRead.py
@@ -624,7 +624,7 @@ class threadRead(ThreadWithStop):
     def _handle_encoder_sample(self, rpm, velocity, distance, stamp=None, publish_ros=None):
         if publish_ros is None:
             publish_ros = self._should_publish_ros_sample()
-        self.currentSpeedSender.send(float(velocity) * float(self._wheel_vel_scale))
+        self.currentSpeedSender.send(float(velocity) * float(self._wheel_vel_scale) * 100.0)
         if publish_ros:
             self._publish_wheel_encoder_and_twist([rpm, velocity, distance], stamp)
 
